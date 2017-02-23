fd42619e340f9262363869a270b6bf4f4d91923f
STUDENT\ad5715l <ad5715l@greenwich.ac.uk>
2017-02-23 11:43:04 +0000

    First Commit

---
+++ Assets/Scripts/SolarSystem/Orbit.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Orbit : MonoBehaviour {
+
+    public GameObject planet;
+    public GameObject myPlanet;
+    public float speed;
+    public float mySpeed;
+
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+        
+
+
+        OrbitAround();
+	
+	}
+
+    void OrbitAround() 
+    {
+        transform.RotateAround(myPlanet.transform.position, Vector3.up, mySpeed * Time.deltaTime);
+
+        transform.RotateAround(planet.transform.position, Vector3.up, speed * Time.deltaTime);
+    }
+}
